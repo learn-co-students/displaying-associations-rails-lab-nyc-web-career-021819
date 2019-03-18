79f04a2fbf9634d7665c36ed06ba6dd8dd3cd8b9
github username <github email address>
2019-03-17 21:27:44 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ # resources :artists
+ get '/artists', to: 'artists#index', as: 'artists'
+ get '/artists/new', to: 'artists#new', as: 'new_artist'
+ resources :artists, only: :create
+ # post '/artists/create',  to: 'artists#create', as: 'create_artist'
+ get '/artists/:id', to: 'artists#show', as: 'artist'
+ get '/artists/:id/edit', to: 'artists#edit', as: 'edit_artist'
+ patch '/artists/:id', to: 'artists#update'
+
+ resources :songs
+
 end
